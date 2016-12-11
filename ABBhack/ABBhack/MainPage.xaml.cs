a7675f0337ae048e44b298f38828d2485107a5f6
DSMP <smugdamian@gmail.com>
2016-12-11 01:07:02 +0100

    przegladarka gotowa

--- ABBhack/ABBhack/MainPage.xaml.cs
+++ ABBhack/ABBhack/MainPage.xaml.cs
@@ -10,7 +10,7 @@ namespace ABBhack
     public partial class MainPage : ContentPage
     {
         public MainPage()
-        {
+        {            
             InitializeComponent();
         }
     }
