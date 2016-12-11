f90e2c13e869d8893cd8c67090d3f4d53178862c
Jakub Rumpel <jakub@rumpel.pl>
2016-12-11 03:56:22 +0100

    Added Device class

---
+++ ABBhack/ABBhack/Data/RestService.cs
@@ -0,0 +1,35 @@
+﻿using ABBhack.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABBhack.Data
+{
+    class RestService
+    {
+        HttpClient client;
+
+        public Device DeviceData { get; protected set; }
+
+        public RestService()
+        {
+            client = new HttpClient();
+        }
+
+        public async void GetDeviceData()
+        {
+            string restUrl = "192.168.4.1";
+            var uri = new Uri(restUrl);
+            var response = await client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                DeviceData = JsonConvert.DeserializeObject<Device>(content);
+            }
+        }
+    }
+}
---
+++ ABBhack/ABBhack/Models/Device.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABBhack.Models
+{
+    class Device
+    {
+        public float Temperature { get; set; }
+        public float Humidity { get; set; }
+        public int FotoResistor { get; set; }
+
+    }
+}
